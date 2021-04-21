2993eea240b0a7bfd61bff993b34b97a0135d851
Andrew Arrow <oneone@gmail.com>
2021-04-21 15:23:37 -0700

    bump

--- nonary/int.go
+++ nonary/int.go
@@ -20,14 +20,22 @@ func (i *Int) Add(a int) *Int {
 	end := len(i.List) - 1
 	i.List[end].Val++
 	if i.List[end].Val > 8 {
-		i.List[end].Val = 0
-		if len(i.List) == 1 {
-			fb := FancyBit{}
-			fb.Val = 1
-			i.List = append([]FancyBit{fb}, i.List...)
-		} else {
-			i.List[0].Val++
-		}
+		i.HandleBump(1)
 	}
 	return i
+}
+
+func (i *Int) HandleBump(limit int) {
+	end := len(i.List) - 1
+	i.List[end].Val = 0
+	if len(i.List) == limit {
+		fb := FancyBit{}
+		fb.Val = 1
+		i.List = append([]FancyBit{fb}, i.List...)
+	} else {
+		i.List[0].Val++
+		if i.List[0].Val > 8 {
+			i.HandleBump(limit + 1)
+		}
+	}
 }
