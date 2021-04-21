24b262903ba034c67f9b9f9799575c27dd464fad
Andrew Arrow <oneone@gmail.com>
2021-04-21 14:47:18 -0700

    more

--- nonary/int.go
+++ nonary/int.go
@@ -17,5 +17,16 @@ func NewInt(a int) *Int {
 }
 
 func (i *Int) Add(a int) *Int {
+	i.List[0].Val++
+	if i.List[0].Val > 8 {
+		i.List[0].Val = 0
+		if len(i.List) == 1 {
+			fb := FancyBit{}
+			fb.Val = 1
+			i.List = append([]FancyBit{fb}, i.List...)
+		} else {
+			i.List[1].Val++
+		}
+	}
 	return i
 }
