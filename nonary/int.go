c1c660edc4d5891f87c10af873b09c089ae757a9
Andrew Arrow <oneone@gmail.com>
2021-04-21 17:18:35 -0700

    words

--- nonary/int.go
+++ nonary/int.go
@@ -1,40 +1,43 @@
 package nonary
 
-type FancyBit struct {
-	Val byte
-}
+/*
+
+  [ 8 ] [ 8 ] [ 8 ]
+  [ 1 ] [ 0 ] [ 0 ] [ 0 ]
+
+  increment yourself and if you spill over,
+	tell digit to your left they are now + 1, u goto 0
+	if there is no digit to your left,
+	there is now, and it's a one
+
+*/
 
 type Int struct {
-	List []FancyBit
+	List []byte
 }
 
 func NewInt(a int) *Int {
 	n := Int{}
-	fb := FancyBit{}
-	fb.Val = 0
-	n.List = []FancyBit{fb}
+	n.List = []byte{0}
 	return &n
 }
-
-// 1 0 0 0 1
-// 1 0 0 1 0
-// 1 0 1 0 0
-// 1 0 1 1 1
-// 1 1 0 0 0
 func (i *Int) Add(a int) *Int {
+	return i
+}
+
+/*
+func (i *Int) Add2(a int) *Int {
 	end := len(i.List) - 1
 	i.List[end].Val++
-	// 8 8, 8, 9
 	if i.List[end].Val > 8 {
-		i.HandleBump(1)
+		i.List[end].Val = 0
+		i.HandleBump(len(i.List))
 	}
 	return i
 }
 
 func (i *Int) HandleBump(limit int) {
-	// 8, 9 - 1
-	// 8, 0
-	if len(i.List) == limit {
+	if len(i.List) == limit && i.List[0].Val == 8 {
 		fb := FancyBit{}
 		fb.Val = 1
 		foo := []FancyBit{}
@@ -45,9 +48,10 @@ func (i *Int) HandleBump(limit int) {
 		}
 		i.List = append([]FancyBit{fb}, foo...)
 	} else {
-		i.List[0].Val++
-		if i.List[0].Val > 8 {
-			i.HandleBump(limit + 1)
+		end := len(i.List) - 1
+		i.List[end].Val++
+		if i.List[end].Val > 8 {
+			i.HandleBump(len(i.List))
 		}
 	}
-}
+}*/
